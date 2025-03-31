2c70aad3a60316dec36ca6afb5ce7241dd6ca4f7
binamrathapa <binamrathapa015@gmail.com>
2025-04-01 00:22:23 +0200

    Adds the user create endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # api defination
   namespace :api, defaults: {formate: :json} do
     namespace :v1 do
-      resources :users, only: [:show]
+      resources :users, only: %i[show create]
     end
   end
 end
