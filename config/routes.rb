88be7830efbc688e91ff948ffa41e1bccd8a1dfe
binamrathapa <binamrathapa015@gmail.com>
2025-04-01 00:34:15 +0200

    Adds update action the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # api defination
   namespace :api, defaults: {formate: :json} do
     namespace :v1 do
-      resources :users, only: %i[show create]
+      resources :users, only: %i[show create update]
     end
   end
 end
