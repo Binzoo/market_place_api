7c8cdcf2a1ba3493f3d898a9a2e3f1190ef7f8d5
binamrathapa <binamrathapa015@gmail.com>
2025-04-01 00:04:47 +0200

    Adds show action to the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # api defination
   namespace :api, defaults: {formate: :json} do
     namespace :v1 do
-      
+      resources :users, only: [:show]
     end
   end
 end
